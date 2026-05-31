5cb7bab4fa284aacc0443f7b08219f0961c3ef68
Mimansak Nepal <mimansaknepal@gmail.com>
2026-05-31 14:42:49 +0545

    Changed some stuff in /current to prevent rate limiting everytime

--- backend/main.py
+++ backend/main.py
@@ -24,19 +24,18 @@ def root():
 
 @app.get("/current")
 def get_current():
+    today = str(date.today())
+    if "current" + today in cache:
+        return cache ["current"+ today]
     url = "https://api.open-meteo.com/v1/forecast"
     params = {
-        "latitude":LUKLA_LAT,
-        "longitude":LUKLA_LON,
-       "current": "temperature_2m,wind_speed_10m,wind_direction_10m,precipitation,cloud_cover,visibility,surface_pressure",        
-        }
-    
-    res = requests.get(url,params = params).json()
-    today = str(date.today())
-    if "current" +today in cache:
-        return cache["current"+ today]
-    result = res.get("current",res)
-    cache ["current"+today] = result
+        "latitude": LUKLA_LAT,
+        "longitude" : LUKLA_LON,
+        "current": "temperature_2m, wind_speed_10m, wind_direction_10,. precipitation,cloud_cover, visivility, surface_pressure",
+    }
+    res = requests.get(url,params = params). json()
+    result = res.get("current", res)
+    cache["current" + today] = result
     return result
 
 @app.get("/predict")
