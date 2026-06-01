51ca750e4c74d60c409022447180a68a13f4f518
Mimansak Nepal <mimansaknepal@gmail.com>
2026-06-01 08:30:49 +0545

    Fixed yet more bugs

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,7 @@ def get_current():
     params = {
         "latitude": LUKLA_LAT,
         "longitude" : LUKLA_LON,
-        "current": "temperature_2m, wind_speed_10m, wind_direction_10, precipitation,cloud_cover, visibility, surface_pressure",
+        "current": "temperature_2m,wind_speed_10m,wind_direction_10m,precipitation,cloud_cover,visibility,surface_pressure",
     }
     res = requests.get(url,params = params). json()
     if "current" not in res:
